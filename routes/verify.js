b64eb556b78b1392af1433f7b469b29ebab7ba9d
Armando <armandonogueirar@gmail.com>
2016-04-24 16:33:30 +0200

    First users aproximation

---
+++ routes/verify.js
@@ -0,0 +1,75 @@
+/**
+ * Created by snooze on 4/21/16.
+ */
+
+var User = require('../models/user');
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('../config.js');
+
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, {
+        expiresIn: 3600
+    });
+};
+
+exports.verifyOrdinaryUser = function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secretKey, function (err, decoded) {
+            if (err) {
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                return next(err);
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        var err = new Error('No token provided!');
+        err.status = 403;
+        return next(err);
+    }
+};
+
+exports.verifyAdminUser = function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secretKey, function (err, decoded) {
+            if (err) {
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                return next(err);
+            } else {
+                // if everything is good, save to request for use in other routes
+                // console.log(decoded);
+                // console.log("User is "+decoded._doc.admin);
+                if (decoded._doc.admin) {
+                    req.decoded = decoded;
+                    next();
+                } else {
+                    var err = new Error('User is not an admin!');
+                    err.status = 403;
+                    return next(err);
+                }
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        var err = new Error('No token provided!');
+        err.status = 403;
+        return next(err);
+    }
+};
